021037843964c7c58f2f2e902646c5d3e0a38a07
Amrish Bharatiya <bamrish@gmail.com>
2019-01-18 17:12:32 +0530

    Added code to create new post and post data to the server

--- src/components/App/Blog/NewPost/NewPost.js
+++ src/components/App/Blog/NewPost/NewPost.js
@@ -1,33 +1,75 @@
-import React from 'react';
-import classes from './NewPost.module.css'
+import React, {Component} from 'react';
+import classes from './NewPost.module.css';
+import axios from 'axios';
 
-const newPost = () => {
+class NewPost extends Component {
+  state = {
+    title: '',
+    body: '',
+    author: ''
+  }
+
+  titleChangeHandler = (event) => {
+    this.setState({title: event.target.value});
+  }
+
+  contentChangeHandler = (event) => {
+    this.setState({body: event.target.value});
+  }
+
+  authorChangeHandler = (event) => {
+    this.setState({author: event.target.value});
+  }
+
+  formSubmitHandler = () => {
+    console.log(this.state);
+    axios.post('https://jsonplaceholder.typicode.com/posts', this.state)
+      .then( (response) => {
+        console.log(response);
+      });
+  }
+
+  render(){
     return (
         <div className={classes.NewPost}>
           <h3 className={classes.title}>Create a New Post</h3>
           <div className={classes.form}>
             <div className={classes["form-row"]}>
               <label htmlFor="postTitle">Title</label>
-              <input name="title" className={classes.postTitle} />              
+              <input name="title" 
+                value={this.state.title} 
+                className={classes.postTitle} 
+                onChange={this.titleChangeHandler}/>              
             </div>
             <div className={classes["form-row"]}>
               <label htmlFor="content">Content</label>
-              <textarea name="content" className={classes.content}></textarea>
+              <textarea name="content" 
+                value={this.state.content}  
+                className={classes.content}
+                onChange={this.contentChangeHandler}>   
+              </textarea>
             </div>
             <div className={classes["form-row"]}>
               <label htmlFor="author">Author</label>
-              <select name="author" className={classes.Author}>
+              <select name="author" 
+                className={classes.Author}
+                value={this.state.author} 
+                onChange={this.authorChangeHandler}>
                 <option value="Amrish">Amrish</option>
                 <option value="Aarushi">Aarushi</option>
                 <option value="Anishka">Anishka</option>
               </select>           
             </div>       
             <div className={classes["form-row"]}>
-                <button className={classes.submitButton}>Create Post</button>
+                <button className={classes.submitButton}
+                  onClick={this.formSubmitHandler}>
+                  Create Post
+                </button>
             </div>     
           </div>
         </div>
     )
+  }
 }
 
-export default newPost;
+export default NewPost;
