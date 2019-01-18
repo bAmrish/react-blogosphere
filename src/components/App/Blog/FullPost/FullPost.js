d936fba6bf462fc4d0d1efa2e98fa0d5eac3fa9e
Amrish Bharatiya <bamrish@gmail.com>
2019-01-18 13:38:17 +0530

    Added a litte styling to Posts and author to FullPost component

--- src/components/App/Blog/FullPost/FullPost.js
+++ src/components/App/Blog/FullPost/FullPost.js
@@ -5,6 +5,7 @@ const fullPost = (props) => {
     return (
         <div className={classes.FullPost}>
             <h2 className={classes.postTitle}>{props.post.title}</h2>
+            <div className={classes.author}>{props.post.author}</div>
             <div className={classes.content}>{props.post.content}</div>
         </div>
     );
